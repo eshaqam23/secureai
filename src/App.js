96e97467f6c885cc95638ee4b19fefcda76fb450
eshaqam23 <eshaqam23@gmail.com>
2024-09-14 00:05:45 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Logo from './Components/Logo';
 import './App.css';
 
 function App() {
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div class="innerHeader">
+          <Logo />
+          <ul class="navigation">
+            <li>Home</li>
+            <li>Demo</li>
+            <li>Team</li>
+            <li>Resources</li>
+          </ul>
+        </div>
       </header>
     </div>
   );
---
+++ src/Components/Logo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Logo () {
+    return (
+        <div class="logo">
+            <div class="logoImg"><img src="../../img/secureAI.png" alt="SecureAI Logo" /></div>
+            <div class="logoName">
+                <h2>SecureAI</h2>
+            </div>
+        </div>
+    );
+}
+
+export default Logo;
+
